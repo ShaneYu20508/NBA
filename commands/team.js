7b6e0758ea790f6e025b556e641929473c154d5c
ShaneYu20508 <grantgmgm@gmail.com>
2023-12-27 16:37:16 +0800

    gitignore

---
+++ commands/team.js
@@ -0,0 +1,78 @@
+// team.js
+import axios from 'axios'
+import cheerio from 'cheerio'
+import template from '../templates/team.js'
+
+export default async function team (event) {
+  if (!event.message || event.message.type !== 'text') {
+    console.log('Invalid message format:', event.message)
+    return
+  }
+
+  const userMessage = event.message.text.trim()
+
+  if (userMessage) {
+    try {
+      // 使用axios獲取表格頁面的HTML
+      const response = await axios.get('https://tw-nba.udn.com/nba/standings/')
+      const data = response.data
+
+      // 使用Cheerio解析HTML
+      const $ = cheerio.load(data)
+
+      // 找到表格的所有行
+      const rows = $('table.stable.matchup.standings tbody tr')
+
+      // 隊伍陣列
+      const teamNames = []
+
+      // 勝場、敗場、勝率陣列
+      const winArray = []
+      const loseArray = []
+      const winRateArray = []
+
+      // 遍歷每一行
+      rows.each((index, row) => {
+        const teamName = $(row).find('td a').text().trim().replace(/^\d+/, '')
+        const win = $(row).find('td:eq(1)').text().trim()
+        const lose = $(row).find('td:eq(2)').text().trim()
+        const winRate = $(row).find('td:eq(3)').text().trim()
+
+        teamNames.push(teamName)
+        winArray.push(win)
+        loseArray.push(lose)
+        winRateArray.push(winRate)
+      })
+
+      // 輸出隊伍名稱和數據檢查是否正確
+      console.log('Team Names:', teamNames)
+      console.log('Wins:', winArray)
+      console.log('Loses:', loseArray)
+      console.log('Win Rates:', winRateArray)
+
+      // 判斷使用者輸入的文字是否與隊伍名稱相對應
+      for (let i = 0; i < teamNames.length; i++) {
+        if (teamNames[i].includes(userMessage)) {
+          // 使用提供的模板生成泡泡
+          const bubble = template(winArray[i], teamNames[i], loseArray[i], winRateArray[i])
+
+          // 回復泡泡模板
+          event.reply({
+            type: 'flex',
+            altText: 'NBA Standings',
+            contents: bubble
+          })
+          return
+        }
+      }
+      const replyMsg = {
+        type: 'text',
+        text: `找不到 ${userMessage} 這支隊伍`
+      }
+      event.reply(replyMsg)
+    } catch (error) {
+      console.error('Error fetching data:', error.message)
+      event.reply('發生錯誤，請稍後再試。')
+    }
+  }
+}
